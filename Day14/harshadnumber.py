caaa0888a2f671a02c637de6b79f74fa531fdbd6
sonasusanm-creator <sonasusanm@gmail.com>
2026-03-10 22:38:20 +0530

    Create harshadnumber.py

---
+++ Day14/harshadnumber.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
+        digit_sum = sum(int(d) for d in str(x))
+        
+        if x % digit_sum == 0:
+            return digit_sum
+        else:
+            return -1
