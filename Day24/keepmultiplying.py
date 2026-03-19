58f223b12fc3285397f1a94d831c27bfe49d625b
sonasusanm-creator <sonasusanm@gmail.com>
2026-03-19 17:52:41 +0530

    Create keepmultiplying.py

---
+++ Day24/keepmultiplying.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findFinalValue(self, nums: list[int], original: int) -> int:
+        num_set = set(nums)
+        
+        while original in num_set:
+            original *= 2
+        
+        return original
